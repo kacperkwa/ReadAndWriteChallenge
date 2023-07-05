06ecc343cc0f760e85fc792df73a492b3bc711d8
kacperkwa <kacperkwartaxz@gmail.com>
2023-07-05 08:26:17 +0200

    Add project files.

---
+++ ReadAndWriteChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+
+namespace ReadAndWriteChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\kacpe\Desktop\C# tutorialsEU\CompleteCsharp\ReadAndWriteChallenge\Assets\input.txt");
+
+
+
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\kacpe\Desktop\C# tutorialsEU\CompleteCsharp\ReadAndWriteChallenge\Assets\output.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    if (line.Contains("split"))
+                    {
+                        string[] splittedLine = line.Split(" ");
+                        string text = splittedLine[4];
+                        file.Write(text + " ");
+                    }
+
+
+                }
+            }
+            Console.WriteLine("Saved a new text file!");
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
